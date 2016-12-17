8702ed5e381fbd8217fff7b4a3fb2833ddaa4d35
Edvard <edvardlindelof@gmail.com>
2016-12-17 16:16:10 +0100

    scalajs-react is in

--- build.sbt
+++ build.sbt
@@ -7,3 +7,16 @@ scalaVersion := "2.11.8"
 libraryDependencies ++= Seq(
   "com.github.japgolly.scalajs-react" %%% "core" % "0.11.3"
 )
+
+jsDependencies ++= Seq(
+
+  "org.webjars.bower" % "react" % "15.3.2"
+    /        "react-with-addons.js"
+    minified "react-with-addons.min.js"
+    commonJSName "React",
+
+  "org.webjars.bower" % "react" % "15.3.2"
+    /         "react-dom.js"
+    minified  "react-dom.min.js"
+    dependsOn "react-with-addons.js"
+    commonJSName "ReactDOM")
--- src/main/scala/minimaljsapp/Main.scala
+++ src/main/scala/minimaljsapp/Main.scala
@@ -1,12 +1,18 @@
 package minimaljsapp
 
 import scala.scalajs.js.JSApp
+import scala.scalajs.js.annotation.JSExport
+import org.scalajs.dom.document
+import japgolly.scalajs.react._
+import japgolly.scalajs.react.vdom.prefix_<^._
 
 object Main extends JSApp {
 
-  // TODO put some root component in here
+  @JSExport
   def main(): Unit = {
     println("GoudaOst")
+    // TODO put some root component in here
+    ReactDOM.render(<.h1("GoudaOst"), document.getElementById("AppRoot"))
   }
 
 }
