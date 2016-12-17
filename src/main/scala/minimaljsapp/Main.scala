70b9e6d37730386dff35c3bc562727734758d5fb
Edvard <edvardlindelof@gmail.com>
2016-12-17 19:35:06 +0100

    navbar switching between SomeComp and SomeOtherComp

--- src/main/scala/minimaljsapp/Main.scala
+++ src/main/scala/minimaljsapp/Main.scala
@@ -11,13 +11,34 @@ object Main extends JSApp {
   @JSExport
   def main(): Unit = {
     println("GoudaOst")
-    // TODO put some root component in here
-    ReactDOM.render(rootComponent("hello from rootComp"), document.getElementById("AppRoot"))
+    ReactDOM.render(RootComp(), document.getElementById("AppRoot"))
   }
 
-  val rootComponent = ReactComponentB[String]("RootComponent")
-    .render_P(str => <.h1(str))
+  // the type of our components after calling build
+  type State = ReactComponentC.ConstProps[Unit, Unit, Unit, org.scalajs.dom.raw.Element]
+
+  class Backend($: BackendScope[Unit, State]) {
+    def changeState(s: State): Callback = $.setState(s)
+    def render(s: State) =
+      <.div(
+        <.ul(
+          <.li("CompA", ^.onClick --> changeState(SomeComp)),
+          <.li("CompB", ^.onClick --> changeState(SomeOtherComp))),
+        s()
+      )
+  }
+
+  val RootComp = ReactComponentB[Unit]("Example")
+    .initialState(SomeComp)
+    .renderBackend[Backend]
     .build
 
+  val SomeComp = ReactComponentB[Unit]("SomeComp")
+    .render_P(_ => <.h2("Hello from SomeComp"))
+    .build
+
+  val SomeOtherComp = ReactComponentB[Unit]("SomeOtherComp")
+    .render_P(_ => <.h2("Hello from SomeOtherComp"))
+    .build
 
 }
