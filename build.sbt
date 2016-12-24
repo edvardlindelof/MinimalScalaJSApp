f484d102a7cee1906d3067d5af1d4485621977a5
Edvard <edvardlindelof@gmail.com>
2016-12-25 00:18:10 +0100

    scalacss

--- build.sbt
+++ build.sbt
@@ -6,6 +6,7 @@ scalaVersion := "2.11.8"
 
 libraryDependencies ++= Seq(
   "com.github.japgolly.scalajs-react" %%% "core" % "0.11.3",
+  "com.github.japgolly.scalacss" %%% "core" % "0.5.1",
   "org.singlespaced" %%% "scalajs-d3" % "0.3.3",
   "fr.hmil" %%% "roshttp" % "2.0.0-RC1"
 )
---
+++ src/main/scala/minimalscalajsapp/MyStyles.scala
@@ -0,0 +1,12 @@
+package minimalscalajsapp
+
+import scalacss.Defaults._
+
+object MyStyles extends StyleSheet.Inline {
+  import dsl._
+
+  val myNavStyle = style(addClassNames("navbar", "navbar-static-top", "navbar-default"))
+  val myNavListSTyle = style(addClassNames("nav", "navbar-nav"))
+  val MyButtonStyle = style(addClassNames("btn", "navbar-btn"))
+
+}
