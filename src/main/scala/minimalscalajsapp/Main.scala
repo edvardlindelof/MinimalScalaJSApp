fc8424abfb453441f82424b1055578085cdc3a17
Edvard Lindelöf <edvardlindelof@gmail.com>
2020-02-24 11:49:36 +0100

    bumped bootstrap to v4, and switched from CSS classes to className strings

--- src/main/scala/minimalscalajsapp/Main.scala
+++ src/main/scala/minimalscalajsapp/Main.scala
@@ -5,6 +5,7 @@ import scala.scalajs.js.annotation.JSExport
 import org.scalajs.dom.document
 import japgolly.scalajs.react._
 import japgolly.scalajs.react.vdom.prefix_<^._
+import com.karasiq.bootstrap4.navbar
 
 import MyStyles._
 
@@ -23,16 +24,20 @@ object Main extends JSApp {
     def render(p: Unit, s: State) =
       <.div(
         <.nav(
-          ^.className := myNavStyle.htmlClass,
-          <.ul(
-            MenuButton("Comp A", SomeComp()),
-            MenuButton("Comp B", SomeOtherComp()),
-            MenuButton("Facade Test", FacadeTest()),
-            MenuButton("json Editor Test", JSONEditorTest()),
-            MenuButton("Widget Injection Test", WidgetInjectionTest()),
-            MenuButton("D3 Test", D3Test()),
-            MenuButton("CSS Test", CSSTest()),
-            MenuButton("Draggable Test", DraggableTest()))),
+          ^.className := "navbar navbar-expand-lg navbar-light bg-light",
+          <.div(
+            ^.className := "collapse navbar-collapse",
+            <.div(
+              ^.className := "navbar-nav",
+              MenuButton("Comp A", SomeComp()),
+              MenuButton("Comp B", SomeOtherComp()),
+              MenuButton("Facade Test", FacadeTest()),
+              MenuButton("json Editor Test", JSONEditorTest()),
+              MenuButton("Widget Injection Test", WidgetInjectionTest()),
+              MenuButton("D3 Test", D3Test()),
+              MenuButton("CSS Test", CSSTest()),
+              MenuButton("Draggable Test", DraggableTest())
+            ))),
         s.component
       )
 
@@ -41,9 +46,10 @@ object Main extends JSApp {
         component(MenuBtnProps(text, element))
       case class MenuBtnProps(text: String, element: ReactElement)
       val component = ReactComponentB[MenuBtnProps]("MenuButton")
-        .render_P(p => <.li(p.text,
+        .render_P(p => <.a(p.text,
+                            ^.className := "nav-item nav-link",
                             ^.onClick --> changeState(p.element),
-                            ^.className := MyButtonStyle.htmlClass))
+                            ))
         .build
     }
   }
--- src/main/scala/minimalscalajsapp/MyStyles.scala
+++ src/main/scala/minimalscalajsapp/MyStyles.scala
@@ -5,6 +5,8 @@ import scalacss.Defaults._
 object MyStyles extends StyleSheet.Inline {
   import dsl._
 
+  // TODO consider removing all three
+  // they're unused atm because the amount of coupling between this and the main file doesnt feel practical
   val myNavStyle = style(addClassNames("navbar", "navbar-static-top", "navbar-default"))
   val myNavListSTyle = style(addClassNames("nav", "navbar-nav"))
   val MyButtonStyle = style(addClassNames("btn", "navbar-btn"))
